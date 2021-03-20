506360cb5869bda2b11f4e391ea5ad4538c08026
lzs <1721973588@qq.com>
2021-03-20 19:32:43 +0800

    更新4

--- hello-git/src/main/java/com/qhit/git/HellloGit.java
+++ hello-git/src/main/java/com/qhit/git/HellloGit.java
@@ -4,5 +4,12 @@ public class HellloGit {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("更新1");
+        int a =10;
+        Integer b =10;
+        Integer c = new Integer(10);
+
+        System.out.println(a==b);
+        System.out.println(a==c);
+        System.out.println(b==c);
     }
 }
